4204cfddcd2de1053e4ff455ed3be527481ff033
Blake Polidore <blakepolidore@gmail.com>
2016-06-08 11:55:22 -0700

    set views, manifest permission

--- MorningAlarm/app/src/main/java/blake/com/morningalarm/MainActivity.java
+++ MorningAlarm/app/src/main/java/blake/com/morningalarm/MainActivity.java
@@ -2,12 +2,24 @@ package blake.com.morningalarm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TimePicker;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TimePicker timePicker;
+    private ToggleButton toggleButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setViews();
+    }
+
+    private void setViews() {
+        timePicker = (TimePicker) findViewById(R.id.timePicker);
+        toggleButton = (ToggleButton) findViewById(R.id.toggleButton);
     }
 }
